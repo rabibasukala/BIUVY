186cf60153cd84ca9fec998a3c1a7a9dbc02be5e
Rabi Basukala <ibarbasukala77@gmail.com>
2024-03-17 21:10:57 +0545

    add:discriptionfile

--- mapApp/models.py
+++ mapApp/models.py
@@ -1,3 +1,15 @@
 from django.db import models
 
 # Create your models here.
+class MapInfo(models.Model):
+    nameOfPace = models.CharField(max_length=500)
+    descriptionOfplace= models.TextField()
+    latitude = models.FloatField()
+    longitude = models.FloatField()
+    image = models.ImageField(upload_to='images/savedimages', default='images/bkt.jpg')
+    icon = models.ImageField(upload_to='images/savedicons', default='images/pin.svg')
+
+
+
+    def __str__(self):
+        return self.nameOfPace
